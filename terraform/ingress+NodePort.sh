a6d2c77908ca7dc2391c6aa8772637789e011472
ShadyYasser2003 <shadyyasser2003@gmail.com>
2025-08-27 20:03:31 +0300

    good

--- terraform/ingress+NodePort.sh
+++ terraform/ingress+NodePort.sh
@@ -43,7 +43,7 @@ kubectl wait --namespace ingress-nginx \
   --timeout=300s
 
 # -------------------------------
-# عرض البيانات
+# عرض البيانات 
 # ------------------------------- 
 echo "📊 Ingress Controller Service:"
 kubectl get svc -n ingress-nginx ingress-nginx-controller
