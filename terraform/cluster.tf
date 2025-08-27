e0511a4bb3610b6c609da54416e80e25c3ed5abe
ShadyYasser2003 <shadyyasser2003@gmail.com>
2025-08-27 18:47:59 +0300

    updates

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -6,8 +6,9 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids              = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
-    endpoint_private_access = true
+    # aws_subnet.private[*].id,
+    subnet_ids              = concat( aws_subnet.public[*].id )
+    # endpoint_private_access = true
     endpoint_public_access  = true
     security_group_ids      = [aws_security_group.eks_nodes_sg.id]
   }
@@ -17,7 +18,7 @@ resource "aws_eks_cluster" "eks" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy,
-    aws_subnet.private,
+    # aws_subnet.private,
     aws_subnet.public,
     aws_security_group.eks_nodes_sg
   ]
